a5df6428696b40173d4672a41f95972524bca95d
Nikita77227 <Nikita77227@DESKTOP-029TR2H>
2022-12-12 18:13:17 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Cube.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    public class Cube
+    {
+        private int _blueCubes = 0;
+        private int _cubes = 1;
+        public int NumberCuts(int cuts)
+        {
+            int finalNumberCuts = 0;
+            int previousNumberCuts = 0;
+            for (int i = 1; i <= cuts; i++)
+            {
+                if (i == 1)
+                {
+                    finalNumberCuts = 3;
+                    previousNumberCuts = finalNumberCuts;
+                }
+                else
+                {
+                    finalNumberCuts += previousNumberCuts;
+                    previousNumberCuts = finalNumberCuts;
+                }
+            }
+            return finalNumberCuts;
+        }
+        public int NumberCubes(int cuts)
+        {
+            _cubes = 1;
+            int counterCubes = 2;
+            for (int j = 1; j <= cuts; j++)
+            {
+                if (j == 1)
+                {
+                    _cubes = counterCubes * counterCubes * counterCubes;
+                }
+                else
+                {
+                    _cubes = counterCubes * counterCubes * counterCubes;
+                }
+                counterCubes++;
+            }
+            return _cubes;
+        }
+        public int NumberBlueCubes(int cuts)
+        {
+            _blueCubes = 0;
+            if (cuts >= 2)
+            {
+                _blueCubes = 1;
+                int counterBlueСenter = 2;
+                int counterBlueEdge = 2;
+                for (int i = 2; i <= cuts - 1; i++)
+                {
+                    _blueCubes = counterBlueСenter * counterBlueСenter * counterBlueСenter;
+                    counterBlueСenter++;
+                }
+            }
+            return _blueCubes;
+        }
+        public int NumberRedCubes(int cuts)
+        {
+            int counterCubes = 2;
+            _cubes = 0;
+            for (int j = 1; j <= cuts; j++)
+            {
+                if (j == 1)
+                {
+                    _cubes = counterCubes * counterCubes * counterCubes;
+                }
+                else
+                {
+                    _cubes = counterCubes * counterCubes * counterCubes;
+                }
+                counterCubes++;
+            }
+            if (cuts >= 2)
+            {
+                _blueCubes = 1;
+                int counterBlueСenter = 2;
+                int counterBlueEdge = 2;
+                for (int i = 2; i <= cuts - 1; i++)
+                {
+                    _blueCubes = counterBlueСenter * counterBlueСenter * counterBlueСenter;
+                    counterBlueСenter++;
+                }
+            }
+            _cubes -= _blueCubes;
+            return _cubes;
+        }
+        //Happy Coding :)
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,57 @@
+﻿using ConsoleApp7;
+using System;
+
+Cube cube = new();
+Console.WriteLine("Синий куб окунули в красную краску и разрезали во весех трёх измерениях\n Выберите что вы хотите узнать");
+void GetMenu()
+{
+    Console.WriteLine();
+    Console.WriteLine($"1) Количество разрезов куба");
+    Console.WriteLine($"2) Количество кубиков получившихся после разрезов");
+    Console.WriteLine($"3) Количество синих кубиков");
+    Console.WriteLine($"4) Количество красных кубиков");
+    Console.WriteLine();
+}
+void UseMenu()
+{
+    while (true)
+    {
+        GetMenu();
+        string choice = Console.ReadLine();
+        bool checkChoice = int.TryParse(choice, out int resultChoice);
+        if (resultChoice > 4)
+        {
+            Console.WriteLine($"Выберите пункт из меню");
+        }
+        else
+        {
+            Console.WriteLine($"Введите сколько раз произвести действие с кубом");
+            string repetition = Console.ReadLine();
+            bool checkRepetition = int.TryParse(repetition, out int resultRepetition);
+            Console.WriteLine();
+            switch (resultChoice)
+            {
+                case 1:
+                    var numberCuts = cube.NumberCuts(resultRepetition);
+                    Console.WriteLine($"Разрезов куба: {numberCuts}");
+                    break;
+                case 2:
+                    var numberCubes = cube.NumberCubes(resultRepetition);
+                    Console.WriteLine($"кубиков: {numberCubes}");
+                    break;
+                case 3:
+                    var numberBlueCubes = cube.NumberBlueCubes(resultRepetition);
+                    Console.WriteLine($"синих кубиков: {numberBlueCubes}");
+                    break;
+                case 4:
+                    var numberRedCubes = cube.NumberRedCubes(resultRepetition);
+                    Console.WriteLine($"красных кубиков: {numberRedCubes}");
+                    break;
+                default:
+                    Console.WriteLine($"Выберите пункт из меню");
+                    break;
+            }
+        }
+    }
+}
+UseMenu();
